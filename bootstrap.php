246e9ce53cb2e9d9f41c49acaccedd45866042ca
吳駿松 <jo32229hn@gmail.com>
2018-10-30 21:49:33 +0800

    修改 bootstrap , 新增composer.json

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,5 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_DATABASE', '');
 define('DB_CHARSET', 'utf8');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
