373aa6a64bd07d5254cb8335131f76d3aa69e00d
吳駿松 <jo32229hn@gmail.com>
2018-10-30 22:16:12 +0800

    bootstrap 修改

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
-define('DB_DATABASE', '');
+define('DB_DATABASE', 'database.sql');
 define('DB_CHARSET', 'utf8');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
